4a7891f3788fb775f3036fd21ffbbc5c1442cad2
sousamarcelo <marcelo.sousa@aluno.faculdadeimpacta.com.br>
2020-10-05 19:21:52 -0300

    Corrigindo marcação dos comentarios

--- src/Exercicio02.java
+++ src/Exercicio02.java
@@ -1,8 +1,8 @@
 import java.util.Scanner;
 
-//Escreva um programa para ler as coordenadas (X,Y) de uma quantidade indeterminada de pontos no sistema
-//cartesiano. Para cada ponto escrever o quadrante a que ele pertence. O algoritmo será encerrado quando pelo
-//menos uma de duas coordenadas for NULA (nesta situação sem escrever mensagem alguma).
+/*Escreva um programa para ler as coordenadas (X,Y) de uma quantidade indeterminada de pontos no sistema
+cartesiano. Para cada ponto escrever o quadrante a que ele pertence. O algoritmo será encerrado quando pelo
+menos uma de duas coordenadas for NULA (nesta situação sem escrever mensagem alguma).*/
 
 public class Exercicio02 {
 
