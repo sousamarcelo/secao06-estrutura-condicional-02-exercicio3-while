440bce3489efc3731306027ffc755fef2eda86bd
sousamarcelo <marcelo.sousa@aluno.faculdadeimpacta.com.br>
2020-10-04 22:31:38 -0300

    Exercicio01 - resolvido

---
+++ src/Exercicio01.java
@@ -0,0 +1,28 @@
+import java.util.Scanner;
+
+//Escreva um programa que repita a leitura de uma senha até que ela seja válida. Para cada leitura de senha
+//incorreta informada, escrever a mensagem "Senha Invalida". Quando a senha for informada corretamente deve ser
+//impressa a mensagem "Acesso Permitido" e o algoritmo encerrado. Considere que a senha correta é o valor 2002.
+
+public class Exercicio01 {
+
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		
+		System.out.print("Digite senha valida: ");
+		int senhaDigitada = sc.nextInt();
+		int senha = 2002;
+		
+		while (senhaDigitada != senha) {
+			System.out.println("Senha Invalida");
+			senhaDigitada = sc.nextInt();
+		}
+		
+		System.out.println("Acesso Permitido");
+		
+		
+		sc.close();
+
+	}
+
+}
